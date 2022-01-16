5f9923a5adf1fda4a902a08ca8f17ccec395c0d1
Lucas Lage <flage.lucas@gmail.com>
2022-01-16 18:31:47 +0100

    changed the fast alignments output file name

--- Assignment_5/get_report.sh
+++ Assignment_5/get_report.sh
@@ -5,7 +5,7 @@ python score-alignments -n 0 < dice.a >> ../results.txt
 echo $'\nIBM Model 1 Performance' >> ../results.txt
 python score-alignments -n 0 < ../IBM_Model1/model1.a >> ../results.txt
 echo $'\nFast align Performance' >> ../results.txt
-python score-alignments -n 0 <  ../fast_align/forward.align >> ../results.txt
+python score-alignments -n 0 <  ../fast_align/fast.a >> ../results.txt
 cd ..
 echo $'\n### Alignments Comparison ###' >> ./results.txt
 echo $'\n### Dice ###' >> ./results.txt
@@ -13,5 +13,5 @@ python align_viz.py -idx 0 -a_path ./hw2/dice.a >> ./results.txt
 echo $'\n### IBM Model 1 ###' >> ./results.txt
 python align_viz.py -idx 0 -a_path ./IBM_Model1/model1.a >> results.txt
 echo $'\n### Fast aligner ###' >> ./results.txt
-python align_viz.py -idx 0 -a_path ./fast_align/forward.align >> results.txt
+python align_viz.py -idx 0 -a_path ./fast_align/fast.a >> results.txt
 
