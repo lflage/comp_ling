8729dc32253b397a852520170ae249b6b6e71147
Lucas Fonseca Lage <lucasflage@hotmail.com>
2022-03-17 20:40:21 +0100

    changed import structure

--- FinalProject/lda_cli.py
+++ FinalProject/lda_cli.py
@@ -1,9 +1,8 @@
-import my_lda as LDA
 import argparse, pickle
+from my_lda import LDA
 
 parser = argparse.ArgumentParser(description=
         'uses')
-
 parser.add_argument('-nTopics', type=int, default=20, help=
         'number of topics to assign to LDA' )
 parser.add_argument('-path', type=str,
@@ -13,10 +12,16 @@ parser.add_argument('-nIter', type=int, default=500,
 
 args = parser.parse_args()
 
-lda = LDA(args.path,Ntopic=args.nIter)
-lda.readCorpus()
-lda.initMatrices()
-lda.runIter(args.nIter)
+with open(args.path+'.out', 'wb') as file:
+    lda = LDA(args.path,Ntopic=args.nIter)
+    lda.readCorpus()
+    lda.initMatrices()
+    lda.runIter(args.nIter)
+    pickle.dump(lda, file)
+
+
+
+
 
 with open(args.path+'.out', 'wb') as file:
     pickle.dump(lda, file)
