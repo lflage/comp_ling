a515d957b101b9adc223b7b30889ea768f727df3
Lucas Fonseca Lage <lucasflage@hotmail.com>
2022-03-18 22:29:01 +0100

    saves only one time now

--- FinalProject/lda_cli.py
+++ FinalProject/lda_cli.py
@@ -9,19 +9,15 @@ parser.add_argument('-path', type=str,
         help='Path to input txt file ')
 parser.add_argument('-nIter', type=int, default=500,
         help='Number of GibbsSampling iterations')
+parser.add_argument('-outPath',type=str, default='./lda',
+	help='Path to output serialized pickle file')
 
 args = parser.parse_args()
 
-with open(args.path+'.out', 'wb') as file:
+with open(args.outPath+'.out', 'wb') as file:
     lda = LDA(args.path,Ntopic=args.nTopics)
     lda.readCorpus()
     lda.initMatrices()
     lda.runIter(args.nIter)
     pickle.dump(lda, file)
 
-
-
-
-
-with open(args.path+'.out', 'wb') as file:
-    pickle.dump(lda, file)
